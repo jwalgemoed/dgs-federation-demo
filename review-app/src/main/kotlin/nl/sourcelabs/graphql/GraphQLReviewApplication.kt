ce58c0c4c2b375b0f512e66a5095e4a601ee3b5a
JWalgemoed <j.walgemoed@sourcelabs.nl>
2022-03-18 13:06:38 +0100

    Some structural changes to the Reviews app

--- review-app/src/main/kotlin/nl/sourcelabs/graphql/GraphQLReviewApplication.kt
+++ review-app/src/main/kotlin/nl/sourcelabs/graphql/GraphQLReviewApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class GraphQLReviewApplication
 
 fun main(args: Array<String>) {
-	runApplication<GraphQLReviewApplication>(*args)
+    runApplication<GraphQLReviewApplication>(*args)
 }
--- review-app/src/main/kotlin/nl/sourcelabs/graphql/federation/model/Review.kt
+++
@@ -1,8 +0,0 @@
-package nl.sourcelabs.graphql.federation.model
-
-data class Review(
-    val id: Long,
-    val albumId: Long,
-    val text: String,
-    val score: Int,
-)
